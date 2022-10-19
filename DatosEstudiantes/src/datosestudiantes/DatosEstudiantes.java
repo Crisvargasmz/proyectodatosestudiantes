21f4a10036eb6c242fed5cb3780a5a0b7fe6bbc5
Crisvargasmz <104032540+Crisvargasmz@users.noreply.github.com>
2022-10-19 06:53:19 -0600

    Add files via upload

--- DatosEstudiantes/src/datosestudiantes/DatosEstudiantes.java
+++ DatosEstudiantes/src/datosestudiantes/DatosEstudiantes.java
@@ -15,7 +15,8 @@ public class DatosEstudiantes {
      * @param args the command line arguments
      */
     public static void main(String[] args) {
-        // TODO code application logic here
+        principalMDI mdi= new principalMDI();
+        mdi.setVisible(true);
     }
     
 }
